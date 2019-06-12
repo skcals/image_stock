58a28ae35426d143ee20d33c73e60dfa1686f3f2
web zone <webzone76@gmail.com>
2019-06-12 18:53:49 +0530

    initial commit

---
+++ precache-manifest.5727755756cb2cc59b98662e0162bc95.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ddfd3e36ce6b6427696",
+    "url": "/css/app.b3368d2e.css"
+  },
+  {
+    "revision": "4ddfd3e36ce6b6427696",
+    "url": "/js/app.715fe603.js"
+  },
+  {
+    "revision": "0569e6e0a9b2512488cb",
+    "url": "/css/chunk-vendors.602d312a.css"
+  },
+  {
+    "revision": "0569e6e0a9b2512488cb",
+    "url": "/js/chunk-vendors.e88c387e.js"
+  },
+  {
+    "revision": "8a9a261c8b8dfe90db11f1817a9d22e1",
+    "url": "/fonts/MaterialIcons-Regular.8a9a261c.woff2"
+  },
+  {
+    "revision": "016c14adad95a0e21a2ae4fbc36955c6",
+    "url": "/fonts/MaterialIcons-Regular.016c14ad.eot"
+  },
+  {
+    "revision": "c38ebd3cd38c98fbd16bf31d1d24ce64",
+    "url": "/fonts/MaterialIcons-Regular.c38ebd3c.woff"
+  },
+  {
+    "revision": "d41ae58dab60e355ae3872ebf2f1403a",
+    "url": "/fonts/MaterialIcons-Regular.d41ae58d.ttf"
+  },
+  {
+    "revision": "e9dbed1d0f12b2a045f655ddc92e1e65",
+    "url": "/img/back.e9dbed1d.jpg"
+  },
+  {
+    "revision": "e8fee44a316d7af3940b0dc1d35f63cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
